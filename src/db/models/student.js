2379620c6bdb97439c2e2f56001fb7719c1f3f1b
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-09-07 12:33:13 +0300

    studentSchema

---
+++ src/db/models/student.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const studentSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  phoneNumber: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: false,
+  },
+  isFavourite: {
+    type: Boolean,
+    default: false,
+  },
+  contactType: {
+    type: String,
+    required: true,
+    enum: ['work', 'home', 'personal'],
+    default: 'personal',
+  },
+});
+
+export const StudentsCollection = model('students', studentSchema); //collection: students
