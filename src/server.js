6832e7ec4d2d43176c8e7e8f921dffda85a9bc59
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-09-09 19:15:56 +0300

    routers and controllers folders

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import pino from 'pino-http';
 import express from 'express';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js'; //Since we export the router using export default, we can import it with any name
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 
 // const PORT = Number(process.env.PORT);
 const PORT = Number(env('PORT', '3000'));
 
 export function setupServer() {
   const app = express();
+  // app.use(express.json());
+
   app.use(
     pino({
       transport: {
@@ -19,33 +21,7 @@ export function setupServer() {
   // CORS middleware
   app.use(cors());
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({ message: 'Contact not found' });
-      return;
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: {
-          contact,
-        },
-      });
-    }
-  });
+  app.use(contactsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
