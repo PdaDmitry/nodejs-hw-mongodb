d66f7587303c4156f95cd973431e90fd6a3cbcd2
PdaDmitry <pda.gazcomp@gmail.com>
2024-09-13 12:16:25 +0300

    Update validation/students.js  contactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,3 +1,12 @@
 import Joi from 'joi';
 
-export const contactSchema = Joi.object({});
+export const contactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20).email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
+});
