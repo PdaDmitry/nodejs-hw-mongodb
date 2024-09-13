1b695467d98676fc4c156685ab6c5bf54e98e942
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-09-13 19:23:55 +0300

    contactUpdateSchemaValidation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -30,3 +30,31 @@ export const contactSchemaValidation = Joi.object({
       'any.required': 'Contact Type is required',
     }),
 });
+
+export const contactUpdateSchemaValidation = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base': 'PhoneNumber should be a string',
+    'string.min': 'PhoneNumber should have at least {#limit} characters',
+    'string.max': 'PhoneNumber should have at most {#limit} characters',
+  }),
+  email: Joi.string().min(3).max(20).email().optional().messages({
+    'string.base': 'Email should be a string',
+    'string.min': 'Email should have at least {#limit} characters',
+    'string.max': 'Email should have at most {#limit} characters',
+    'any.required': 'Email must be a valid email address',
+  }),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .messages({
+      'string.base': 'Contact Type should be a string',
+      'any.only': 'Contact Type must be one of {#valids}',
+      'any.required': 'Contact Type is required',
+    }),
+});
