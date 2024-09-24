f2faf4062916799b91bf9e00dfb71269f9889530
PdaDmitry <pda.gazcomp@gmail.com>
2024-09-24 12:14:42 +0300

    Update services\auth.js  Hashing password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,11 @@
+import bcrypt from 'bcrypt';
 import { User } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
-  return await User.create(payload);
+  const encryptedPassword = await bcrypt.hash(payload.password, 10); //Hashing passwords
+
+  return await User.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
